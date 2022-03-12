5371b19794213e26488264a932ec8ec33b2d4c68
AsatiHimanshu <hasati@asu.edu>
2022-03-12 13:23:06 -0700

    updated whitenoise

--- DensecServer/settings.py
+++ DensecServer/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
